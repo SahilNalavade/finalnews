e20ae55be9cfc1e372e5ddb0f4e7c8cf2755e812
SahilNalavade <84601587+SahilNalavade@users.noreply.github.com>
2024-04-26 12:55:23 +0530

    Update settings.py

--- news/settings.py
+++ news/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     
     
 ]
@@ -127,3 +128,4 @@ STATICFILES_DIRS=[
 ]
 STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
