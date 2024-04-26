af27bb68858375846a1d7cddaec8b4c7ebab9314
SahilNalavade <84601587+SahilNalavade@users.noreply.github.com>
2024-04-26 11:32:52 +0530

    Update urls.py

--- news/urls.py
+++ news/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('newsui/', include("newsui.urls")),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
